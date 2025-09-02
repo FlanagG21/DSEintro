c77755e4c859393b267588c3bf366a7be688a4b9
Galen Flanagan <flanagg21@vt.edu>
2025-09-02 15:27:04 +0000

    added test_Kronecker

---
+++ test_Kronecker.py
@@ -0,0 +1,10 @@
+import pytest
+from Kronecker import kroneckerProduct
+
+def testKronecker():
+    u = [[0,1]]
+    v =[[1,0]]
+    s = [[1,1],[1,0]]
+    t = [[0,1],[1,0]]
+    assert kroneckerProduct(u,v) == [[0,0,1,0]]
+    assert kroneckerProduct(s,t) == [[0,1,0,1],[1,0,1,0],[0,1,0,0],[1,0,0,0]]
